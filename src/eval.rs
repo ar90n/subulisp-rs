1f3670ea49757d8d822953e11599cb9d4c7908e5
Masahiro Wada <argon.argon.argon@gmail.com>
2023-01-02 12:36:33 +0000

    feat: add evn to context

--- src/eval.rs
+++ src/eval.rs
@@ -1,12 +1,39 @@
+use std::collections::HashMap;
+
 use super::expr::Expr;
 
 #[derive(Debug)]
-pub(crate) struct Context {}
+pub(crate) struct Context<'a> {
+    env: HashMap<String, Expr>,
+    parent: Option<&'a Context<'a>>,
+}
 
-impl Context {
+impl<'a> Context<'a> {
     #[allow(dead_code)]
     fn new() -> Self {
-        Self {}
+        Self {
+            env: HashMap::new(),
+            parent: None,
+        }
+    }
+
+    pub fn resolve(&self, name: &str) -> Option<&Expr> {
+        self.env
+            .get(name)
+            .or_else(|| self.parent.as_ref().and_then(|p| p.resolve(name)))
+    }
+
+    #[allow(dead_code)]
+    pub fn register(&mut self, name: &str, expr: Expr) {
+        self.env.insert(name.to_string(), expr);
+    }
+
+    #[allow(dead_code)]
+    pub fn with(&'a self, env: HashMap<String, Expr>) -> Context<'a> {
+        Self {
+            env,
+            parent: Some(self),
+        }
     }
 }
 
@@ -45,6 +72,13 @@ pub(crate) fn evaluate(expr: Expr, ctx: &Context) -> anyhow::Result<Expr> {
                     }
                 }
                 _ => anyhow::bail!("failed to evaluate list: invalid function: {:?}", func),
+            }
+        }
+        Expr::Symbol(s) => {
+            if let Some(expr) = ctx.resolve(&s) {
+                Ok(expr.clone())
+            } else {
+                anyhow::bail!("failed to evaluate symbol: unknown symbol: {}", s)
             }
         }
         _ => Ok(expr),
@@ -175,11 +209,13 @@ mod built_in {
 
 #[cfg(test)]
 mod test {
+    use std::hash::Hash;
+
     use super::*;
 
     #[test]
     fn test_evaluate() {
-        let ctx = Context::new();
+        let mut ctx = Context::new();
         assert_eq!(
             Expr::Number(-3.25),
             evaluate(
@@ -255,6 +291,25 @@ mod test {
                 &ctx
             )
             .unwrap()
+        );
+
+        ctx.register("A", Expr::Number(1.0));
+        ctx.register("B", Expr::Number(2.0));
+        assert_eq!(
+            Expr::Number(1.0),
+            evaluate(Expr::Symbol("A".to_string()), &ctx).unwrap()
+        );
+
+        let mut env2 = HashMap::new();
+        env2.insert("A".to_string(), Expr::Number(3.0));
+        let ctx2 = ctx.with(env2);
+        assert_eq!(
+            Expr::Number(3.0),
+            evaluate(Expr::Symbol("A".to_string()), &ctx2).unwrap()
+        );
+        assert_eq!(
+            Expr::Number(2.0),
+            evaluate(Expr::Symbol("B".to_string()), &ctx2).unwrap()
         );
     }
 }
