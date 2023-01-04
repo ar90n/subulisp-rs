56833ee6213d46eb06966a950a9f774b512a1db1
Masahiro Wada <argon.argon.argon@gmail.com>
2023-01-04 12:17:19 +0000

    feat: add repl loop

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
+use std::io::Write;
+
 use subulisp_rs::Lisp;
 
 fn main() {
     let mut env = Lisp::new();
-    let result = env.evaluate("(+ 1 2)".to_string()).unwrap();
-    println!("{}", result);
+    loop {
+        print!("> ");
+        std::io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        if !input.trim().is_empty() {
+            let result = env.evaluate(input).unwrap();
+            println!("{}", result);
+        }
+    }
 }
